44c6ed461c59271e3e5960c4ec60649d93b6ffc6
sociotorn <sociotorn@gmail.com>
2024-06-22 22:45:48 -0500

    Reorganized SAMARA_HSD and added new submodules

--- fetchCMSIS.cmake
+++
@@ -1,9 +0,0 @@
-include(FetchContent)
-
-FetchContent_Declare(
-    CMSIS
-    GIT_REPOSITORY https://github.com/Sociotorn/CMSIS_Core_CMake.git
-    GIT_TAG develop
-)
-
-FetchContent_MakeAvailable(CMSIS)
--- tests/teensy/testStartup.c
+++ tests/teensy/testStartup.c
@@ -1,9 +1,12 @@
 // #include "GPIO.h"
+#include "MIMXRT1062.h"
 
 int main()
 {
     // GPIO_Init();
     // GPIO_Write(LED_PIN,HIGH);
+    GPIO2->GDIR   |= 1 << 3;
+    GPIO2->DR_SET |= 1 << 3;
 
     return 0;
 }
